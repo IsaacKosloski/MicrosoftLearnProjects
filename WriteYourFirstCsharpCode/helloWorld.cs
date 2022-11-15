79f08263df981c84015ff46613bfb5b4d046cd2d
Isaac <isaac.kosloski@ufms.br>
2022-11-15 09:18:58 -0300

    helloWorld

---
+++ WriteYourFirstCsharpCode/helloWorld.cs
@@ -0,0 +1,17 @@
+//Hello World with C#
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            //Console.WriteLine("Hello World!");
+
+            Console.Write("Congratulations!");
+            Console.Write(" ");
+            Console.Write("You wrote your first lines of code!");
+            Console.ReadKey();
+        }
+    }
+}
